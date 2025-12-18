3761868d23f5f8d1e4a693e6820289c858bacd1b
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-12-18 21:01:23 +0700

    buat model data DataSiswa dengan serialisasi

--- app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/pertemuan12/modeldata/DataSiswa.kt
@@ -10,23 +10,3 @@ data class DataSiswa(
     val telpon: String
 )
 
-data class UIStateSiswa(
-    val detailSiswa: DetailSiswa = DetailSiswa(),
-    val isEntryValid: Boolean = false
-)
-
-data class DetailSiswa(
-    val id: Int = 0,
-    val nama: String = "",
-    val alamat: String = "",
-    val telpon: String = ""
-)
-
-// Fungsi ini WAJIB ada agar EntryViewModel tidak error
-fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
-    id = id, nama = nama, alamat = alamat, telpon = telpon
-)
-
-fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
-    id = id, nama = nama, alamat = alamat, telpon = telpon
-)
