6a6b0cdade626278d63b3ece9154d97176fa9f3a
harismuhammad0912 <muhammadharis091204@gmail.com>
2025-12-18 21:17:18 +0700

    implementasi logika update state dan simpan data pada EntryViewModel

--- app/src/main/java/com/example/pertemuan12/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/viewmodel/EntryViewModel.kt
@@ -10,3 +10,19 @@ import com.example.pertemuan12.modeldata.toDataSiswa
 import com.example.pertemuan12.repositori.RepositoryDataSiswa
 
 class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = detailSiswa.nama.isNotBlank() && detailSiswa.alamat.isNotBlank() && detailSiswa.telpon.isNotBlank()
+        )
+    }
+
+    suspend fun addSiswa() {
+        if (uiStateSiswa.isEntryValid) {
+            repositoryDataSiswa.insertDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+        }
+    }
+}
